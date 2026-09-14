25fd927ac8cbe2f3700e527600cd1dd99f17133e
ziro-lab <320528831+ziro-lab@users.noreply.github.com>
2026-09-14 22:51:02 +0900

    W7: initialize expression preset WPF surface

---
+++ src/Ymm4TemplatePlacer/ExpressionPresetPanel.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows.Controls;
+
+namespace Ymm4TemplatePlacer;
+
+public partial class ExpressionPresetPanel : UserControl
+{
+    public ExpressionPresetPanel() => InitializeComponent();
+}
