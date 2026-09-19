626129b121cfa5a8bbd53f46be212d69f695c090
ziro-lab <320528831+ziro-lab@users.noreply.github.com>
2026-09-19 15:05:26 +0900

    R2-E proof: import WPF UIElement for single-click route

--- tests/NativeHandsOnWorkflowProof.cs
+++ tests/NativeHandsOnWorkflowProof.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using System.Reflection;
 using System.Text.Json;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using YukkuriMovieMaker.Project;
