b7b7aeea5ec9f096c881d61bd91b940cdc475894
ziro-lab <320528831+ziro-lab@users.noreply.github.com>
2026-09-21 01:47:57 +0900

    Tests: import VisualTreeHelper for responsive grid proof

--- tests/NativeHandsOnRound4PresentationProof.cs
+++ tests/NativeHandsOnRound4PresentationProof.cs
@@ -5,6 +5,7 @@ using System.Windows.Automation.Peers;
 using System.Windows.Automation.Provider;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using YukkuriMovieMaker.Project;
 using YukkuriMovieMaker.Project.Items;
 using YukkuriMovieMaker.UndoRedo;
