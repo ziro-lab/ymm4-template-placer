5763195048491bd99bf6f00169e7df7d22a2e1a1
ziro-lab <320528831+ziro-lab@users.noreply.github.com>
2026-09-23 11:22:22 +0900

    Run placement source P6 resync proof

--- tests/NativeV04Proof.cs
+++ tests/NativeV04Proof.cs
@@ -12,6 +12,7 @@ internal static partial class NativeProof
         await VerifyPlacementSourcePresetMaterialization(timeline);
         await VerifyPlacementSourceNormalTiles(timeline, undo);
         await VerifyPlacementSourceExpressions(timeline, undo);
+        await VerifyPlacementSourceResync(timeline, undo);
         var settingsField = typeof(PlacerViewModel).GetField("settings", BindingFlags.Instance | BindingFlags.NonPublic)!;
         var bootstrapped = (PlacerSettings)settingsField.GetValue(ViewModel!)!;
         Assert(bootstrapped.Palettes.Count == 0 && bootstrapped.Library.All(x => bootstrapped.ImportedExpressionSources.Contains(x.Source)),
