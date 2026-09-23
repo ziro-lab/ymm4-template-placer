e797f5169b46277bcce65438c9f365911ac2add2
ziro-lab <320528831+ziro-lab@users.noreply.github.com>
2026-09-23 10:06:02 +0900

    Run placement source P0 proof

--- tests/NativeV04Proof.cs
+++ tests/NativeV04Proof.cs
@@ -7,6 +7,7 @@ internal static partial class NativeProof
     private static async Task VerifyV04(Timeline timeline, UndoRedoManager undo)
     {
         VerifyPortableSettingsStorage();
+        VerifyPlacementSourceModel(timeline);
         var settingsField = typeof(PlacerViewModel).GetField("settings", BindingFlags.Instance | BindingFlags.NonPublic)!;
         var bootstrapped = (PlacerSettings)settingsField.GetValue(ViewModel!)!;
         Assert(bootstrapped.Palettes.Count == 0 && bootstrapped.Library.All(x => bootstrapped.ImportedExpressionSources.Contains(x.Source)),
