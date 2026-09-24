b54fa8ad72df0c1111c24c85758b877086f6b15c
ziro-lab <320528831+ziro-lab@users.noreply.github.com>
2026-09-25 07:56:17 +0900

    Select the applicable quick settings page

--- src/Ymm4TemplatePlacer/PanelQuickSettingsPanel.xaml.cs
+++ src/Ymm4TemplatePlacer/PanelQuickSettingsPanel.xaml.cs
@@ -4,5 +4,17 @@ namespace Ymm4TemplatePlacer;
 
 public partial class PanelQuickSettingsPanel : UserControl
 {
-    public PanelQuickSettingsPanel() => InitializeComponent();
+    public PanelQuickSettingsPanel()
+    {
+        InitializeComponent();
+        Loaded += (_, _) => SelectDefaultPage();
+        DataContextChanged += (_, _) => { if (IsLoaded) SelectDefaultPage(); };
+    }
+
+    internal void SelectDefaultPage()
+    {
+        QuickSettingsTabs.SelectedItem = DataContext is PlacerViewModel { HasPlacementQuickDraft: true }
+            ? PlacementQuickTab
+            : PresentationQuickTab;
+    }
 }
